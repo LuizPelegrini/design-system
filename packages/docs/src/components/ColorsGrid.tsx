012b79352543985865418daf69648f5631f2a1ae
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2023-10-22 14:35:43 +0800

    creates docs for color tokens

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,24 @@
+import { colors } from '@stamblew/tokens'
+import { getContrast } from 'polished'
+
+export function ColorsGrid() {
+  return (
+    <>
+      {Object.entries(colors).map(([key, color]) => (
+        <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              fontFamily: 'monospace',
+              color: getContrast('white', color) > 4.5 ? 'white' : 'black',
+            }}
+          >
+            <strong>${key}</strong>
+            <span>{color}</span>
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
